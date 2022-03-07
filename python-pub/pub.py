877bc44fbcaae511d6454894f7d231f5d2381980
mahesh <madu140@gmail.com>
2022-03-07 08:14:03 +0530

    pub added

---
+++ python-pub/pub.py
@@ -0,0 +1,162 @@
+# Espresso Pattern - from example
+# This shows how to capture data using a pub-sub proxy
+
+from __future__ import print_function
+import binascii
+import os
+from random import randint
+import zmq
+
+def socket_set_hwm(socket, hwm=-1):
+    """libzmq 2/3/4 compatible sethwm"""
+    try:
+        socket.sndhwm = socket.rcvhwm = hwm
+    except AttributeError:
+        socket.hwm = hwm
+
+def dump(msg_or_socket):
+    """Receives all message parts from socket, printing each frame neatly"""
+    if isinstance(msg_or_socket, zmq.Socket):
+        # it's a socket, call on current message
+        msg = msg_or_socket.recv_multipart()
+    else:
+        msg = msg_or_socket
+    print("----------------------------------------")
+    for part in msg:
+        print("[%03d]" % len(part), end=' ')
+        is_text = True
+        try:
+            print(part.decode('ascii'))
+        except UnicodeDecodeError:
+            print(r"0x%s" % (binascii.hexlify(part).decode('ascii')))
+
+def set_id(zsocket):
+    """Set simple random printable identity on socket"""
+    identity = u"%04x-%04x" % (randint(0, 0x10000), randint(0, 0x10000))
+    zsocket.setsockopt_string(zmq.IDENTITY, identity)
+
+def zpipe(ctx):
+    """build inproc pipe for talking to threads
+    mimic pipe used in czmq zthread_fork.
+    Returns a pair of PAIRs connected via inproc
+    """
+    a = ctx.socket(zmq.PAIR)
+    b = ctx.socket(zmq.PAIR)
+    a.linger = b.linger = 0
+    a.hwm = b.hwm = 1
+    iface = "inproc://%s" % binascii.hexlify(os.urandom(8))
+    a.bind(iface)
+    b.connect(iface)
+    return a,b
+
+import time
+
+from random import randint
+from string import ascii_uppercase as uppercase
+from threading import Thread
+
+import zmq
+from zmq.devices import monitored_queue
+
+# import zpipe as zpipe
+
+# The subscriber thread requests messages starting with
+# A and B, then reads and counts incoming messages.
+
+def subscriber_thread():
+    ctx = zmq.Context.instance()
+
+    # Subscribe to "A" and "B"
+    subscriber = ctx.socket(zmq.SUB)
+    subscriber.connect("tcp://localhost:6001")
+    subscriber.setsockopt(zmq.SUBSCRIBE, b"A")
+    subscriber.setsockopt(zmq.SUBSCRIBE, b"B")
+    subscriber.setsockopt(zmq.SUBSCRIBE, b"C")
+
+    count = 0
+    while count < 5:
+        try:
+            msg = subscriber.recv_multipart()
+            dump(msg)
+        except zmq.ZMQError as e:
+            if e.errno == zmq.ETERM:
+                break           # Interrupted
+            else:
+                raise
+        count += 1
+
+    print ("Subscriber received %d messages" % count)
+
+
+# publisher thread
+# The publisher sends random messages starting with A-J:
+
+def publisher_thread():
+    ctx = zmq.Context.instance()
+
+    publisher = ctx.socket(zmq.PUB)
+    publisher.bind("tcp://*:6000")
+
+    while True:
+        time.sleep(1)
+        string = "%s-%s" % (uppercase[randint(0,10)], randint(0,10000000000000000000000000000000000000000000000000000000000000000000000000))
+        try:
+            publisher.send(string.encode('utf-8'))
+        except zmq.ZMQError as e:
+            if e.errno == zmq.ETERM:
+                break           # Interrupted
+            else:
+                raise
+        time.sleep(0.1)         # Wait for 1/10th second
+
+
+# listener thread
+# The listener receives all messages flowing through the proxy, on its
+# pipe. Here, the pipe is a pair of ZMQ_PAIR sockets that connects
+# attached child threads via inproc. In other languages your mileage may vary:
+
+def listener_thread (pipe):
+
+    # Print everything that arrives on pipe
+    while True:
+        try:
+            print (pipe.recv_multipart())
+        except zmq.ZMQError as e:
+            if e.errno == zmq.ETERM:
+                break           # Interrupted
+
+
+# main thread
+# The main task starts the subscriber and publisher, and then sets
+# itself up as a listening proxy. The listener runs as a child thread:
+
+def main ():
+
+    # Start child threads
+    ctx = zmq.Context.instance()
+    p_thread = Thread(target=publisher_thread)
+    s_thread = Thread(target=subscriber_thread)
+    p_thread.start()
+    s_thread.start()
+
+    pipe = zpipe(ctx)
+
+    subscriber = ctx.socket(zmq.XSUB)
+    subscriber.connect("tcp://localhost:6000")
+
+    publisher = ctx.socket(zmq.XPUB)
+    publisher.bind("tcp://*:6001")
+
+    l_thread = Thread(target=listener_thread, args=(pipe[1],))
+    l_thread.start()
+
+    try:
+        monitored_queue(subscriber, publisher, pipe[0], b'pub', b'sub')
+    except KeyboardInterrupt:
+        print ("Interrupted")
+
+    del subscriber, publisher, pipe
+    ctx.term()
+
+if __name__ == '__main__':
+    main()
