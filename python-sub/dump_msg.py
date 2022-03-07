541ca9007c2e6d34fcde67ab6267a1752f51931e
mahesh <madu140@gmail.com>
2022-03-07 07:56:18 +0530

    msg dump added

---
+++ python-sub/dump_msg.py
@@ -0,0 +1,25 @@
+import binascii
+import zmq
+import os
+
+
+def dump(msg_or_socket):
+
+    """Receives all message parts from socket, printing each frame neatly"""
+    if isinstance(msg_or_socket, zmq.Socket):
+        # it's a socket, call on current message
+        msg = msg_or_socket.recv_multipart()
+    else:
+        msg = msg_or_socket
+
+    print("----------------------------------------")
+
+
+    for part in msg:
+        print("[%03d]" % len(part), end=' ')
+        is_text = True
+        try:
+            print(part.decode('ascii'))
+        except UnicodeDecodeError:
+            print(r"0x%s" % (binascii.hexlify(part).decode('ascii')))
+
