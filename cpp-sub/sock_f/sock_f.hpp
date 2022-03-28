e494ec27fb5d273182bb657f75a52fab81afec30
newark <madu140@gmail.com>
2022-03-28 07:33:59 +0530

    socket class refactored

--- cpp-sub/sock_f/sock_f.hpp
+++ cpp-sub/sock_f/sock_f.hpp
@@ -3,23 +3,34 @@
 #ifndef SOCK_H
 #define SOCK_H
 #include <zmq.hpp>
+
 namespace IPCPYCPPZMQSOCK
 {
     class sock_f
     {
+
         private:
             zmq::socket_t *socket = ZMQ_NULLPTR;
+
         public:
             sock_f(/* args */);
             ~sock_f();
+            void bindSocketAddres();
+            void getSocketOption(int option_);
+            void setSocketOption(int option_,const void *optionValue, size_t optionValueLength);
+    
     };
+
 } // namespace IPCPYCPPZMQSOCK
 
 IPCPYCPPZMQSOCK::sock_f::sock_f()
 {
     this->socket = new zmq::socket_t();
 }
+
 IPCPYCPPZMQSOCK::sock_f::~sock_f()
 {
+
 }
+
 #endif
