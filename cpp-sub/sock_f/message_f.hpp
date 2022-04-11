63da22ebb413491c7ef0aed47b18b187940da49d
newark <madu140@gmail.com>
2022-04-11 08:38:36 +0530

    message class added

---
+++ cpp-sub/sock_f/message_f.hpp
@@ -0,0 +1,20 @@
+// encapsulate message
+// encapsulate all processes realated to message
+// user only need to specify message that in the intsererst of theirs
+
+class message_f
+{
+private:
+    /* data */
+public:
+    message_f(/* args */);
+    ~message_f();
+};
+
+message_f::message_f(/* args */)
+{
+}
+
+message_f::~message_f()
+{
+}
