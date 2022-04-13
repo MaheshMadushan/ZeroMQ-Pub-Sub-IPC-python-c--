3098a54f4515af85d57579f201fc53738552c508
newark <madu140@gmail.com>
2022-04-13 09:52:38 +0530

    message added

--- cpp-sub/sock_f/message_f.hpp
+++ cpp-sub/sock_f/message_f.hpp
@@ -2,17 +2,19 @@
 // encapsulate all processes realated to message
 // user only need to specify message that in the intsererst of theirs
 
+#include <zmq.hpp>
 class message_f
 {
-private:
-    /* data */
-public:
-    message_f(/* args */);
-    ~message_f();
+    private:
+        zmq::message_t message  = nullptr;
+    public:
+        message_f();
+        ~message_f();
 };
 
 message_f::message_f(/* args */)
 {
+    zmq::message_t 
 }
 
 message_f::~message_f()
