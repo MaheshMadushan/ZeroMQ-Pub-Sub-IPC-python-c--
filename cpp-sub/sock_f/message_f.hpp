5e168f20bc770f7c3ffd669ee233dcd800c91f13
newark <madu140@gmail.com>
2022-04-26 07:46:17 +0530

    design decision added

--- cpp-sub/sock_f/message_f.hpp
+++ cpp-sub/sock_f/message_f.hpp
@@ -1,8 +1,26 @@
 // encapsulate message
 // encapsulate all processes realated to message
 // user only need to specify message that in the intsererst of theirs
-
+// keep follwing dunction in mind. this is in socket.
+// send_result_t send(message_t &msg, send_flags flags)
+//             {
+//                 int nbytes = zmq_msg_send(msg.handle(), _handle, static_cast<int>(flags));
+//                 if (nbytes >= 0)
+//                     return static_cast<size_t>(nbytes);
+//                 if (zmq_errno() == EAGAIN)
+//                     return {};
+//                 throw error_t();
+//             }
 #include <zmq.hpp>
+#define IPCPYCPPZMQ_DELETE(p_object)  \
+    {                                 \
+        if(p_object == ZMQ_NULLPTR){  \
+            return;                   \
+        }                             \
+        delete p_object;              \
+        p_object = 0;                 \
+    }
+
 class message_f
 {
     private:
@@ -11,13 +29,11 @@ class message_f
         message_f();
         ~message_f();
 };
-
 message_f::message_f()
 {
-    message = new zmq::message_t();
+   this->message = new zmq::message_t();
 }
-
 message_f::~message_f()
 {
-
+    IPCPYCPPZMQ_DELETE(this->message);
 }
