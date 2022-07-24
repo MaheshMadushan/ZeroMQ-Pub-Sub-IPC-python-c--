1cb0260c266fbacd6e1ba63e0280892dcaaeaba7
newark <madu140@gmail.com>
2022-07-24 17:12:48 +0530

    refactored

--- cpp-sub/sock_f/message_f.hpp
+++ cpp-sub/sock_f/message_f.hpp
@@ -1,8 +1,6 @@
 // encapsulate message
 // encapsulate all processes realated to message
-// user only need to specify message that in the intsererst of theirs
-// keep follwing dunction in mind. this is in socket.
-// send_result_t send(message_t &msg, send_flags flags)
+// user only need to specify message that in the intsererst of theirs .keep follwing dunction in mind. this is in socket.send_result_t send(message_t &msg, send_flags flags)
 //             {
 //                 int nbytes = zmq_msg_send(msg.handle(), _handle, static_cast<int>(flags));
 //                 if (nbytes >= 0)
@@ -21,7 +19,13 @@ class message_f
     
     public:
         message_f(void *data);
-        message_f(message_f &&rhs) noexcept : message(rhs.message);
+        // gets and r-value (no mem address. like num 6 or stirng 'v')
+        // Rvalue references allow programmers to avoid logically unnecessary copying 
+        // and to provide perfect forwarding functions. 
+        message_f(message_f &&rhs) noexcept : message(rhs.message)
+        {
+            
+        };
         ~message_f();
 };
 
