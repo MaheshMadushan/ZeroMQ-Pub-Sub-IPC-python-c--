dd51e5276346d42a48552ad092925be0ec2b9d13
newark <madu140@gmail.com>
2022-08-30 16:50:10 +0530

    IPC_CREATE_NEW_OBJECT_OF added

--- cpp-sub/utilities/utilities.h
+++ cpp-sub/utilities/utilities.h
@@ -10,6 +10,15 @@
         p_object = 0;                 \
     }
 
+#define IPC_CREATE_NEW_OBJECT_OF(class, out) {                                     \
+        try {                                                               \
+            out = new class;                                                \
+        } catch (std::bad_alloc & ba) {                                     \
+            printf("new c1 failed: %s\n", ba.what());                       \
+            return 1;                                                       \
+        }                                                                   \
+    }
+
 #ifndef NDEBUG
 #define IPC_ASSERT(expression) assert(expression)
 #endif
