533b2b5c5f6b2dc053f9dbbbe3cf81e5cd2a6b76
newark <madu140@gmail.com>
2022-10-10 10:22:30 +0530

    Refactored utilities.h

--- cpp-sub/utilities/utilities.h
+++ cpp-sub/utilities/utilities.h
@@ -1,7 +1,7 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
-#define IPC_DELETE(p_object)  \
+#define IPC_DELETE(p_object)          \
     {                                 \
         if(p_object == ZMQ_NULLPTR){  \
             return;                   \
@@ -9,8 +9,7 @@
         delete p_object;              \
         p_object = 0;                 \
     }
-
-#define IPC_CREATE_NEW_OBJECT_OF(class, out) {                                     \
+#define IPC_CREATE_NEW_OBJECT_OF(class, out) {                              \
         try {                                                               \
             out = new class;                                                \
         } catch (std::bad_alloc & ba) {                                     \
@@ -18,7 +17,6 @@
             return 1;                                                       \
         }                                                                   \
     }
-
 #ifndef NDEBUG
 #define IPC_ASSERT(expression) assert(expression)
 #endif
